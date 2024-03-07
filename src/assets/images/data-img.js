83e3fa5b362e779d68cfe178f939a63307c02f86
Anderson Blas <andi_1516@hotmail.com>
2024-03-07 15:03:23 -0500

    Añadir api de imagenes

---
+++ src/assets/images/data-img.js
@@ -0,0 +1,71 @@
+const dataImg = [
+    {
+        id: 1,
+        imgURL: "https://images.falabella.com/v3/assets/blt9ac1b5ba571392f3/blt03261c54cbbea88f/657b7bd066acfa427f322d39/ON_VITRINA_1_PE_AO.jpg?format=webp&quality=70&width=100p"
+    },
+    {
+        id: 2,
+        imgURL: "https://images.falabella.com/v3/assets/blt9ac1b5ba571392f3/blt3fd265155a4d072a/657b7c9947b3bfc98b387304/ON_VITRINA_2_PE_AO.jpg?format=webp&quality=70&width=100p"
+    },
+    {
+        id: 3,
+        imgURL: "https://images.falabella.com/v3/assets/blt9ac1b5ba571392f3/bltd00379246f3ee5f0/657b7d239f329da781af0df5/ON_VITRINA_3_PE_AO.jpg?format=webp&quality=70&width=100p"
+    },
+    {
+        id: 4,
+        imgURL: "https://images.falabella.com/v3/assets/blt9ac1b5ba571392f3/blt45fcd6b4330677fb/657b7dbc9337911166dbb4b7/ON_VITRINA_4_PE_AO.jpg?format=webp&quality=70&width=100p"
+    }
+];
+
+const dataImgMid = [
+    {
+        id: 1,
+        imgURL: "https://falabella.scene7.com/is/image/FalabellaPE/19641549_1?wid=180&hei=180&qlt=70"
+    },
+    {
+        id: 2,
+        imgURL: "https://falabella.scene7.com/is/image/FalabellaPE/18801353_1?wid=180&hei=180&qlt=70"
+    },
+    {
+        id: 3,
+        imgURL: "https://falabella.scene7.com/is/image/FalabellaPE/17839337_1?wid=180&hei=180&qlt=70"
+    },
+    {
+        id: 4,
+        imgURL: "https://falabella.scene7.com/is/image/FalabellaPE/882727669_1?wid=180&hei=180&qlt=70"
+    },
+    {
+        id: 5,
+        imgURL: "https://falabella.scene7.com/is/image/FalabellaPE/19519469_1?wid=180&hei=180&qlt=70"
+    },
+    {
+        id: 6,
+        imgURL: "https://sodimac.scene7.com/is/image/SodimacPeru/4085175_01?wid=180&hei=180&qlt=70"
+    },
+    {
+        id: 7,
+        imgURL: "https://sodimac.scene7.com/is/image/SodimacPeru/258090X_01?wid=180&hei=180&qlt=70"
+    },
+    {
+        id: 8,
+        imgURL: "https://falabella.scene7.com/is/image/FalabellaPE/18973198_1?wid=180&hei=180&qlt=70"
+    },
+    {
+        id: 9,
+        imgURL: "https://falabella.scene7.com/is/image/FalabellaPE/18273139_1?wid=180&hei=180&qlt=70"
+    },
+    {
+        id: 10,
+        imgURL: "https://sodimac.scene7.com/is/image/SodimacPeru/8977356_01?wid=180&hei=180&qlt=70"
+    },
+    {
+        id: 11,
+        imgURL: "https://falabella.scene7.com/is/image/FalabellaPE/18566267_1?wid=180&hei=180&qlt=70"
+    },
+    {
+        id: 12,
+        imgURL: "https://falabella.scene7.com/is/image/FalabellaPE/19895618_1?wid=180&hei=180&qlt=70"
+    }    
+];
+
+export {dataImg, dataImgMid};
