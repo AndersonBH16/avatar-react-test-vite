b26de3cb11a2525f826f52a1f88ed64344867977
Anderson Blas <andi_1516@hotmail.com>
2024-03-07 16:20:04 -0500

    Direccionar a otra pagina al hacer click en el banner

--- src/components/CarouselUpper/CarouselSlideImage/CarouselSlideImage.jsx
+++ src/components/CarouselUpper/CarouselSlideImage/CarouselSlideImage.jsx
@@ -1,11 +1,14 @@
+import { Link } from 'react-router-dom';
 import './carouselSlideImage.css';
 
 const CarouselSlideImage = ({src, className}) => {
     return(
-        <img
-            src={src}
-            className={className}
-        />
+        <Link to={"https://linio.falabella.com.pe/linio-pe/category/cat50678/Computadoras?sid=SIS-LIPE_Vitrina3_Always_On_W51_computo_"}>
+            <img
+                src={src}
+                className={className}
+            />
+        </Link>        
     );
 };
 
